194dc7d0f1f4dcaeff4481f3de6ad2fbd2760e46
Darwin Quiñones <darwinquinones735@gmail.com>
2022-09-26 20:07:41 -0500

    Challenge finished

---
+++ reto1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// EJERCICIO 1
+Console.WriteLine("Hello, gime your name: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Your name is: "+nombre);
+
+
+// EJERCICIO 2
+
+DateTime fecha = DateTime.Now;
+Console.WriteLine($"The datetime is: {fecha}");
